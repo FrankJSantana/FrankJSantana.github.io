fd533aa4277a05c9190eddbfbb5917d4d144db87
FrankJSantana <fjimenezsantana1@gmail.com>
2026-02-03 04:22:06 +0100

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site : 'https://FrankJSantana.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
